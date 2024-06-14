206275288e1ec94798520cbe39b017d2b27e7d1c
Joy Mia <joymia74@gmail.com>
2024-06-15 02:39:29 +0600

    init

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+import "../scss/bundle.scss";
+
+
+
+window.$ = window.jQuery = require('jquery');
+require('popper.js');
+require('bootstrap');
+require('@fortawesome/fontawesome-free/js/all');
+let mixitup = require('mixitup');
+
+// mixitContainer
+var mixer = mixitup('.mixitContainer');
